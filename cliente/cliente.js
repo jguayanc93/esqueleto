842f01a3632805a005abe58b65ba3cf113b72313
juan carlos <jguayancastillo21@gmail.com>
2025-07-12 13:54:49 -0500

    termina para el domingo la estructura del esqueleto

--- cliente/cliente.js
+++ cliente/cliente.js
@@ -7,17 +7,21 @@ const jws=require('jws')
 //////ESPACIO PARA FUNCIONES DE COMPROBACION PARA LOS QUERYS
 let {bcli_ruc} = require('../querys/cliente/c_buscar_cliente');
 
-let {} = require('../querys/producto');
+let {} = require('../querys/cliente');
 //////////////////////////////////////
 
 router.use(express.json(),express.urlencoded({extended:true}));
 
 ///piensa en una manera de pedir el vendedor por varias entradas y con diferentes condiciones
 router.get('/:id',bcli_ruc)
+//////////VER LA CONDICIONAL DEL PRODUCTO SUMADO A ESTA RUTA
+router.get('/hp/:tinta',prdcodi)
 
-router.get('/hp',prdcodi)
+router.get('/hp/:ploter',prdcodi)
+////////REVISAR SI LA CATEGORIA DEBERIA IR JUNTO CON EL DESCUENTO O EN 2 RUTAS SEPARADAS
+router.get('/categoria',prdcodi)
 
-// router.get('/:cuota',prdcodi)
+router.get('/dsct',prdcodi)
 
 // router.get('/:sugerido',prdcodi)
 

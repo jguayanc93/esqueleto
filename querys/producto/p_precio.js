5b0dacb36ca5917b00f7f039594b58eed4c39da5
juan carlos <jguayancastillo21@gmail.com>
2025-08-02 14:13:49 -0500

    terminar en casa el producto identificado

--- querys/producto/p_precio.js
+++ querys/producto/p_precio.js
@@ -1,67 +1,80 @@
 require('dotenv').config();
+
 const {config,Connection,Request,TYPES} = require('../../conexion/cadena')
+const {conn} = require('../../conexion/cnn')
+
+const {producto} = require('../../id_nombres/lista')
+const asignador_identificadores = require('../../funciones/asignador_indice_nombre')
+
 const {objevacio,objepropiedades} = require('../../funciones/objvacio')
 const {query_validador,objeto_verificador_mejorado} = require('../../funciones/param_verificador')
 
-let bprd_id = (req,res,next) => {
-    console.log("param",req.params);
-    console.log("query",req.query);
+async function bprd_id(req,res,next) {
+    try{
+        const primera_llamada= await obtenerpromesa_conexion();
+        const segunda_llamada= await obtenerpromesa_consulta1(req,primera_llamada);
+        const tercera_llamada= asignador_identificadores(segunda_llamada,producto,1)
+        res.status(200).json(tercera_llamada);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
+
+function obtenerpromesa_conexion(){ return new Promise((resolve,reject)=>conn(resolve,reject)) }
+
+function obtenerpromesa_consulta1(req,conexion){
+    return new Promise((resolve,reject)=>query_prd(resolve,reject,req,conexion))
+}
+
+// let bprd_id = (req,res,next) => {
+//     console.log("param",req.params);
+//     console.log("query",req.query);
     
-    /////////NUEVO METODO FUNCIONAL
-    if(!objevacio(req.params)){
-        let parametros=objepropiedades(req.params);////VALIDO HASTA AQUI
-        let validados=objeto_verificador_mejorado(req.params,parametros,1,"id");
-        if(validados==="valido"){
-            /////validar la longuitud tambien del parametro pasado
-            if(req.params["id"].length===11){
-                bd_conexion(res,req.params.id);
-            }
-            else{
-                res.status(400).send("parametro invalido");
-            }
-        }
-        else{
-            res.status(400).send("parametro invalido");
-        }
-    }
-    else{
-        res.status(400).send("parametro invalido");
-    }
-}
+//     /////////NUEVO METODO FUNCIONAL
+//     if(!objevacio(req.params)){
+//         let parametros=objepropiedades(req.params);////VALIDO HASTA AQUI
+//         let validados=objeto_verificador_mejorado(req.params,parametros,1,"id");
+//         if(validados==="valido"){
+//             /////validar la longuitud tambien del parametro pasado
+//             if(req.params["id"].length===11){
+//                 bd_conexion(res,req.params.id);
+//             }
+//             else{
+//                 res.status(400).send("parametro invalido");
+//             }
+//         }
+//         else{
+//             res.status(400).send("parametro invalido");
+//         }
+//     }
+//     else{
+//         res.status(400).send("parametro invalido");
+//     }
+// }
 
 
-
-let bd_conexion=(res,id)=>{
-    conexion = new Connection(config);
-    conexion.connect();
-    conexion.on('connect',(err)=>{
-        if(err){
-            console.log("ERROR: ",err);
-        }
-        else{
-            bd_c_query(res,id);
-        }
-    });
-}
-
-let bd_c_query = (res,id)=>{
+let query_prd = (resolve,reject,req,conexion)=>{
     let sp_sql="select codi,codf,descr,marc,(CAST(stoc as int)-(CAST(svta as int)+CAST(pedi as int))),vvus,Usr_001,codmar,Usr_016 from prd0101 where codi=@ide";
         
     let consulta = new Request(sp_sql,(err,rowCount,rows)=>{
         if(err){
             /////validar la respuesta en de error de servidor
             conexion.close();
-            res.status(500).send("error interno");
+            // res.status(500).send("error interno");
+            reject("error request");
         }
         else{
             conexion.close();
             if(rows.length==0){
                 /////validar la respuesta en caso de no encontrar nada
-                res.status(500).send("sin resultados?");
+                // res.status(500).send("sin resultados?");
+                reject("no registro");
             }
             else{
                 let respuesta=[];
-                let respuesta2={};
+                // let respuesta2={};
                 let contador=0;
                 rows.forEach(fila=>{
                     let tmp={};
@@ -72,8 +85,9 @@ let bd_c_query = (res,id)=>{
                     })
                     respuesta.push(tmp);
                 });
-                Object.assign(respuesta2,respuesta);
-                res.status(200).json(respuesta2);
+                // Object.assign(respuesta2,respuesta);
+                // res.status(200).json(respuesta2);
+                resolve(respuesta);
             }
         }
     })
